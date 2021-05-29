85749787026a35ed05fefbc667b06e59b623c681
Jules Morley <juliamorley16@gmail.com>
2021-05-29 19:32:19 +0200

    HTML, CSS, Javascript additions

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*--features carousel--*/
+$('.features-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        }
+    }
+});
